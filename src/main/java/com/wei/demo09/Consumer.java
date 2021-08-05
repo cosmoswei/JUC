f0217e1350fe81c27c218936d44d2945f249c570
CosmosWei <2117008741@qq.com>
2021-08-05 17:26:40 +0800

    master

---
+++ src/main/java/com/wei/demo09/Consumer.java
@@ -0,0 +1,14 @@
+package com.wei.demo09;
+
+import com.lmax.disruptor.WorkHandler;
+
+/**
+ * @author cosmoswei
+ */
+public class Consumer implements WorkHandler<PCData> {
+    @Override
+    public void onEvent(PCData pcData) throws Exception {
+
+        System.out.println(Thread.currentThread().getId() + " : Event : --" + pcData.getValue() * pcData.getValue() + "--");
+    }
+}
---
+++ src/main/java/com/wei/demo09/PCData.java
@@ -0,0 +1,15 @@
+package com.wei.demo09;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author cosmoswei
+ */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class PCData {
+    private long value;
+}
